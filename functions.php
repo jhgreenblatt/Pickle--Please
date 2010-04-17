b37c0418c33735e0efb0bf5116a6e9861da97736
Jonathan Greenblatt <jhgreenblatt@gmail.com>
2010-04-17 12:40:42 -0400

    4.13 update

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@
 
 //adding the top utility bar as an action hook-----------------------------------------------------------
 
-function childtheme_top_utility() {
+function pickle_top_utility() {
 ?>
 
 <!Ð the utility html starts here Ð>
@@ -50,27 +50,48 @@ function childtheme_top_utility() {
 
 // Now we add our new function to our Thematic Action Hook
 
-add_action('thematic_before','childtheme_top_utility');
+add_action('thematic_before','pickle_top_utility');
 
 //adding a div around the postheader--------------------------------------------------------------------
 
 
-      function childtheme_posttitle($posttitle) {
+      //function childtheme_posttitle($posttitle) {
 
-       return '<div class="containing">' . $posttitle . '</div>';
-      }
-
-      add_filter('thematic_postheader_posttitle','childtheme_posttitle');
-      
-// Unhook default Thematic functions----(doesn't work >.<)----------------------------------------------------------------
-
-      //function unhook_thematic_functions() {
-
-          // Don't forget the position number if the original function has one
- 
-          //remove_action('thematic_postheader','thematic_entry-content');
+       //return '<div class="containing">' . $posttitle . '</div>';
       //}
-      //add_action('init','unhook_thematic_functions');
+
+      //add_filter('thematic_postheader_posttitle','childtheme_posttitle');
+
+//------------------------------------------------------------------------------creating a thumb loop (seeMirnaOSDF09)
+//* First we will add the thumbnail feature *//
+add_theme_support('post-thumbnails');
+ 
+//* To create our own loop we have to get rid of thematic index loop first.*//
+function remove_index_loop() {
+     remove_action('thematic_indexloop', 'thematic_index_loop');
+}
+add_action('init', 'remove_index_loop');
+ 
+// Now we will create our own loop.
+function thumb_index_loop(){
+       while ( have_posts() ) : the_post()  // Start the loop:
+    // This is just what we decide to show in each post ?>
+    <div id="post-<?php the_ID() ?>" class="<?php thematic_post_class() ?>">
+        <?php the_post_thumbnail();  // we just called for the thumbnail ?>
+        <div class="entry-content">
+             <!-- Display the Title as a link to the Post's permalink. -->
+            <h2 class="entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+            <?php the_meta(); ?>
+            <?php the_content('Read more...'); ?>         
+        </div>
+    </div><!-- .post -->
+    <?php
+    endwhile; // loop done, go back up 
+}
+// And in the end activate the new loop.
+add_action('thematic_indexloop', 'thumb_index_loop');
+
+
 
 
 ?>
